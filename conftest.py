2ba738d07d41cb8fdeaa1e613682c1923790f167
Alexander Cheremisov <dadic2169@mail.ru>
2024-11-01 21:06:16 +0300

    modifieds files conftest.py,test_main_page.py and new file allure_results

--- conftest.py
+++ conftest.py
@@ -13,3 +13,5 @@ def driver():
     driver.quit()
 
 
+
+
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,6 @@
 import allure
 from page_objects.main_page import MainPage
-from conftest import driver
+from conftest import *
 from data import TestData
 import pytest
 
